b8b1f032278f4e35acba8de5acba327a48d00a42
Criss.dev <criss.dev.01@gmail.com>
2023-11-30 07:31:51 -0500

    ready responsive

--- src/components/product/detail/information/current/ProductDetailInformationCurrent.tsx
+++ src/components/product/detail/information/current/ProductDetailInformationCurrent.tsx
@@ -11,40 +11,45 @@ import styles from './ProductDetailInformationCurrent.module.css'
 
 const ProductDetailInformationCurrent = () => {
   return (
-    <Stack spacing={1} direction="row" width="100%">
-      <Image
-        src="/product/product-1-alt.png"
-        className={styles.productImageAlt}
-        alt="product-1-alt"
-        height={134}
-        width={134}
-      />
-      <Stack className={styles.productContentAlt}>
-        <Stack direction="row" justifyContent="space-between" width="100%">
-          <Typography className={styles.productTitleAlt}>Clarifion Air Ionizer</Typography>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <Typography className={styles.productDiscountAlt} variant="caption">
-              $180
-            </Typography>
-            <Typography className={styles.productPriceAlt} variant="body1">
-              $84
-            </Typography>
+    <Stack width="100%">
+      <Stack spacing={1} direction="row" width="100%">
+        <Image
+          src="/product/product-1-alt.png"
+          className={styles.productImageAlt}
+          alt="product-1-alt"
+          height={134}
+          width={134}
+        />
+        <Stack className={styles.productContentAlt}>
+          <Stack direction="row" justifyContent="space-between" width="100%">
+            <Typography className={styles.productTitleAlt}>Clarifion Air Ionizer</Typography>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Typography className={styles.productDiscountAlt} variant="caption">
+                $180
+              </Typography>
+              <Typography className={styles.productPriceAlt} variant="body1">
+                $84
+              </Typography>
+            </Stack>
           </Stack>
+          <Stack direction="row" spacing={1}>
+            <IconStar color="#FFC000" width={18} />
+            <IconStar color="#FFC000" width={18} />
+            <IconStar color="#FFC000" width={18} />
+            <IconStar color="#FFC000" width={18} />
+            <IconStar color="#FFC000" width={18} />
+          </Stack>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>12 left in Stock</li>
+          </ul>
+          <Typography className={styles.comment} variant="body1">
+            Simply plug a Clarifion into any standard outlet and replace bulky, expensive air purifiers with a simple.
+          </Typography>
         </Stack>
-        <Stack direction="row" spacing={1}>
-          <IconStar color="#FFC000" width={18} />
-          <IconStar color="#FFC000" width={18} />
-          <IconStar color="#FFC000" width={18} />
-          <IconStar color="#FFC000" width={18} />
-          <IconStar color="#FFC000" width={18} />
-        </Stack>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>12 left in Stock</li>
-        </ul>
-        <Typography className={styles.comment} variant="body1">
-          Simply plug a Clarifion into any standard outlet and replace bulky, expensive air purifiers with a simple.
-        </Typography>
       </Stack>
+      <Typography className={`${styles.comment} ${styles.commentResponsive}`} variant="body1">
+        Simply plug a Clarifion into any standard outlet and replace bulky, expensive air purifiers with a simple.
+      </Typography>
     </Stack>
   )
 }
--- src/components/product/detail/information/shipping/ProductDetailInformationShipping.tsx
+++ src/components/product/detail/information/shipping/ProductDetailInformationShipping.tsx
@@ -13,12 +13,12 @@ const ProductDetailInformationShipping = () => {
         <Stack>
           <Typography variant="caption">Free shipping</Typography>
         </Stack>
-        <Divider orientation="vertical" variant="middle" flexItem color="#CFCFCF" />
+        <Divider className={styles.divider} orientation="vertical" variant="middle" flexItem color="#CFCFCF" />
         <Stack direction="row" alignItems="center" gap={1}>
           <IconLock color="#CFCFCF" />
           <Typography variant="caption">Secure 256-bit SSL encryption.</Typography>
         </Stack>
-        <Divider orientation="vertical" variant="middle" flexItem color="#CFCFCF" />
+        <Divider className={styles.divider} orientation="vertical" variant="middle" flexItem color="#CFCFCF" />
         <Stack direction="row">
           <Image src="/cards/visa.png" alt="visa-icon" width={24} height={14} />
           <Image src="/cards/shopPay.png" alt="shop-pay-icon" width={24} height={14} />
